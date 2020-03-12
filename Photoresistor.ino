2a45f9465d57a330cc053bc99471a8853fa58791
wiseman1301 <60402972+wiseman1301@users.noreply.github.com>
2020-03-12 20:04:11 +0200

    Add files via upload

---
+++ Photoresistor.ino
@@ -0,0 +1,16 @@
+int phPin =0;
+int ledPin =8;
+
+void setup() {
+ analogReference(DEFAULT);
+ pinMode(ledPin,OUTPUT);
+
+ 
+
+}
+
+void loop() {
+  int ph = analogRead(phPin);
+  if (ph < 200) digitalWrite(ledPin, LOW);
+  else digitalWrite(ledPin, HIGH);
+}
---
+++ Pyrosensor.ino
@@ -0,0 +1,16 @@
+int pyrPin =1;
+int ledPin =8;
+
+void setup() {
+ analogReference(DEFAULT);
+ pinMode(ledPin,OUTPUT);
+
+ 
+
+}
+
+void loop() {
+  int val = analogRead(pyrPin);
+  if (val > 119) digitalWrite(ledPin, HIGH);
+  else digitalWrite(ledPin, LOW);
+}
