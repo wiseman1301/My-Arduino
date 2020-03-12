ac06b6aafe6fbe7b5c906b9a0d1c0dfaaae201f4
wiseman1301 <60402972+wiseman1301@users.noreply.github.com>
2020-03-12 20:02:46 +0200

    Add files via upload

---
+++ Photo-pyro.ino
@@ -0,0 +1,21 @@
+int photoPin =0;
+int pirPin =1;
+int ledPin =9;
+
+
+void setup() {
+ analogReference(DEFAULT);
+ pinMode(ledPin,OUTPUT);
+ 
+
+}
+
+void loop() {
+ int pr = analogRead(pirPin);
+ int ph = analogRead(photoPin);
+  if (ph < 200 && pr > 158) digitalWrite(ledPin, HIGH);
+  else digitalWrite(ledPin, LOW);
+   
+  
+  
+}
