9c95edfd9a1cce99a30fd3487e2553d6f35e4ca8
wiseman1301 <60402972+wiseman1301@users.noreply.github.com>
2020-03-12 20:01:01 +0200

    Add files via upload

---
+++ day-night.ino
@@ -0,0 +1,20 @@
+int phPin =0;
+int day_ledPin =3;
+int night_ledPin =4; 
+
+void setup() {
+ analogReference(DEFAULT);
+ pinMode(day_ledPin,OUTPUT);
+ pinMode(night_ledPin,OUTPUT);
+ 
+
+}
+
+void loop() {
+  int ph = analogRead(phPin);
+  if (ph < 200) digitalWrite(night_ledPin, HIGH);
+  else digitalWrite(night_ledPin, LOW);
+  if (ph >= 200) digitalWrite(day_ledPin, HIGH);
+  else digitalWrite(day_ledPin, LOW);
+ 
+}
